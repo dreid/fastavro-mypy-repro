2b00ecbabf79fd303579630698953ccada0b4b61
dreid <dreid@pilot.com>
2024-02-14 11:35:45 -0800

    Simplify example further.

--- repro.py
+++ repro.py
@@ -1,11 +1,26 @@
 from __future__ import annotations
 
 import fastavro
+import fastavro.read
+from fastavro._read import reader
+from fastavro._read_py import reader as py_reader
 
 reveal_type(fastavro.reader)
 
 def x(reader: fastavro.reader) -> None:
-    print(reader.schema)
+    ...
 
-with open("foo.avro", "rb") as f:
-    x(fastavro.reader(f))
+reveal_type(fastavro.read.reader)
+
+def y(reader: fastavro.read.reader) -> None:
+    ...
+
+reveal_type(reader)
+
+def z(reader: reader) -> None:
+    ...
+
+reveal_type(py_reader)
+
+def zz(reader: py_reader) -> None:
+    ...
